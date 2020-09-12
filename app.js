e9af9c773453fa01afcd845f03c70a7c5e10dc33
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-09-12 16:59:34 +0700

    add routing using express

--- app.js
+++ app.js
@@ -5,22 +5,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
-
-app.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>')
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.get('/', (req, res, next) => {
-    res.send('<h1>Home</h1>');
-});
+app.use('/', (req, res, next) => res.status(404).send('<h1>404: Page not found</h1>'));
 
 const server = http.createServer(app);
 
