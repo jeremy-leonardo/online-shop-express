2cba9687dfee8271e73465bbba1e32439214ba39
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-09-12 18:41:48 +0700

    add style and static files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,12 +10,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.get("/favicon.ico", (req, res) => res.sendStatus(204));
-app.use('/', (req, res, next) => res.status(404).send('<h1>404: Page not found</h1>'));
+app.get("/favicon.ico", (req, res, next) => res.sendStatus(204));
+app.use('/', (req, res, next) => res.status(404).sendFile(path.join(__dirname, 'views', '404.html')));
 
 const server = http.createServer(app);
 
